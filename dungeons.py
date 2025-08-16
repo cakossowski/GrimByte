41fdbd66a758bf6de5f7185e92e0850647e1311d
cakossowski <59179770+cakossowski@users.noreply.github.com>
2025-08-16 21:42:24 +0200

    - created dungeons.py - created DungeonRoom class to use for later generation

---
+++ dungeons.py
@@ -0,0 +1,20 @@
+class DungeonRoom:
+    def __init__(self, name, type_, description, blocked):
+        self.name = name
+        self.type_ = type_
+        self.monsters = []
+        self.description = description
+        self.blocked = blocked
+        self.treasures = []
+
+
+    def __str__(self):
+        return (f"---Dungeon Room---"
+                f"NAME: {self.name} \n"
+                f"TYPE: {self.type_} \n"
+                f"DESCRIPTION: {self.description} \n"
+                f"BLOCKED: {self.blocked}\n"
+                f"MONSTERS {self.monsters} \n"
+                f"TREASURES: {self.treasures} \n"
+                f"---END OF ROOM---")
+
