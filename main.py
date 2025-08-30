7bc4406d41304c05d9d2ff0429b110e4c25c3998
cakossowski <59179770+cakossowski@users.noreply.github.com>
2025-08-30 21:08:39 +0200

    - testing few concepts on main.py module

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import state_machine
 import generator
+import chars
+import movement
+from movement import check_target_destination, move_player_to_target_destination
 
 item_pool = []
 weapon_pool = []
@@ -12,6 +15,17 @@ def main():
     state_machine.create_pools_for_startup(item_pool, weapon_pool, encounter_pool, dungeon_pool)
     generator.generate_map(new_map, dungeon_pool, 5, 3)
     generator.render_map(new_map)
+
+    new_player = chars.PlayerChar("Horst", "player", 100, 5, 5, 1)
+    new_map[0][0].entities.append(new_player)
+    while True:
+        target = input("Direction: \n")
+        new_position = check_target_destination(new_player, target, new_map)
+        if new_position:
+            move_player_to_target_destination(new_player, new_position, new_map)
+
+
+
 
 if __name__ == "__main__":
     main()
