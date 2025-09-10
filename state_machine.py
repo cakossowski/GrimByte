c277bc6149fedd6fc429e072322505d29ad39b5f
Christopher Kossowski <59179770+cakossowski@users.noreply.github.com>
2025-09-10 15:52:03 +0200

    - changed state_machine.py and first steps taken for proper state machine

--- state_machine.py
+++ state_machine.py
@@ -1,12 +1,6 @@
-import chars
-import items
-import dungeons
-import generator
+class GameState:
+    def __init__(self):
+        self.state = "explore"
 
-def create_pools_for_startup(item_pool, weapon_pool, encounter_pool, dungeon_pool):
-    generator.generate_treasure_pool(item_pool)
-    generator.generate_weapon_pool(weapon_pool)
-    generator.generate_encounter_pool(encounter_pool)
-    generator.generate_dungeon_pool(dungeon_pool)
-    return item_pool, weapon_pool, encounter_pool, dungeon_pool
-
+    def handle_input(self, cmd, arg):
+        pass
